a5476552c4507b560e50fc6a0cf0b3a078257e26
Robert Tavares <r.anderson@seazone.com.br>
2024-07-24 02:06:40 -0400

    feat: Adds all files related with the tech interview

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-5xl font-bold font">Desafio Técnico Seazone</h1>
+    </div>
+  );
+}
+
+export default App;
